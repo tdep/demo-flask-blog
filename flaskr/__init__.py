d96d9e47aeb7b00e643dcdb373291de9c8dbc772
Trevor DePew <t.depewmusic@gmail.com>
2023-07-06 16:42:40 -0400

    setting up the db

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,7 +6,7 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        DECRET_KEY='dev',
+        SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
